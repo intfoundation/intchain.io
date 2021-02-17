bbcf25a4675439aece8a7da810cbe3d245477083
like <likeaixi@gmail.com>
2021-02-17 13:41:15 +0800

    update ios download link

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -54,7 +54,7 @@
               <p>{{ $t("pages.wallet.iosWalletDesc") }}</p>
               <a
                 class="download-grid__link button-link"
-                href="https://testflight.apple.com/join/OwhEEKCJ"
+                href="https://testflight.apple.com/join/xTUrGuNZ"
                 target="_blank"
                 >{{ $t("pages.wallet.iosWalletLink") }}</a
               >
